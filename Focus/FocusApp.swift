bfd4773227cf7b80229a8ac10a70fc2e2a012047
Jacob Pantuso <jacobpantuso@outlook.com>
2022-10-22 17:44:53 -0400

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Jacob Pantuso on 2022-10-22.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
